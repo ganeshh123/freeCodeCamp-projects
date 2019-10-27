19ad1f0a7c8caaa83e46c4696f614453045c321c
Ganesh <ganeshh123@users.noreply.github.com>
2019-10-27 20:39:36 +0000

    Working Formula

--- JavaScript Algorithms and Data Structures/5-Cash Register.js
+++ JavaScript Algorithms and Data Structures/5-Cash Register.js
@@ -18,6 +18,34 @@ function checkCashRegister(price, cash, cid) {
         return { status: "CLOSED", change: cid };
     }
 
+    if (changeDue >= 0.25 && changeDue <= cid[3][1]) {
+        var quantity = Math.floor(changeDue / 0.25);
+        var remainder = Math.floor(changeDue % 0.25);
+        change.push(["QUARTER", quantity * 0.25]);
+        changeDue = remainder;
+    }
 
-    return change;
+    if (changeDue >= 0.1 && changeDue <= cid[2][1]) {
+        var quantity = Math.floor(changeDue / 0.1);
+        var remainder = Math.floor(changeDue % 0.1);
+        change.push(["DIME", quantity * 0.1]);
+        changeDue = remainder;
+    }
+
+    if (changeDue >= 0.05 && changeDue <= cid[1][1]) {
+        var quantity = Math.floor(changeDue / 0.05);
+        var remainder = Math.floor(changeDue % 0.05);
+        change.push(["NICKEL", quantity * 0.05]);
+        changeDue = remainder;
+    }
+
+    if (changeDue >= 0.01 && changeDue <= cid[0][1]) {
+        var quantity = Math.floor(changeDue / 0.01);
+        var remainder = Math.floor(changeDue % 0.01);
+        change.push(["PENNY", quantity * 0.01]);
+        changeDue = remainder;
+    }
+
+
+    return { status: "OPEN", change: change };
 }
