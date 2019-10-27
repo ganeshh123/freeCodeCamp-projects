99614d99ecfb85c62a6ad43fa3b1699862c6dfca
Ganesh <ganeshh123@users.noreply.github.com>
2019-10-27 21:19:58 +0000

    Almost Working

--- JavaScript Algorithms and Data Structures/5-Cash Register.js
+++ JavaScript Algorithms and Data Structures/5-Cash Register.js
@@ -18,33 +18,97 @@ function checkCashRegister(price, cash, cid) {
         return { status: "CLOSED", change: cid };
     }
 
+
+    if (changeDue >= 100 && changeDue <= cid[8][1]) {
+        var quantity = Math.floor((changeDue / 100));
+        change.push(["ONE HUNDRED", quantity * 100]);
+        changeDue = changeDue - quantity * 100;
+    } else if (changeDue >= 100) {
+        change.push(["ONE HUNDRED", cid[8][1]]);
+        changeDue -= cid[8][1];
+    }
+    changeDue = parseFloat(changeDue).toFixed(2);
+
+    if (changeDue >= 20 && changeDue <= cid[7][1]) {
+        var quantity = Math.floor((changeDue / 20));
+        change.push(["TWENTY", quantity * 20]);
+        changeDue = changeDue - quantity * 20;
+    } else if (changeDue >= 20) {
+        change.push(["TWENTY", cid[7][1]]);
+        changeDue -= cid[7][1];
+    }
+    changeDue = parseFloat(changeDue).toFixed(2);
+
+    if (changeDue >= 10 && changeDue <= cid[6][1]) {
+        var quantity = Math.floor((changeDue / 10));
+        change.push(["TEN", quantity * 10]);
+        changeDue = changeDue - quantity * 10;
+    } else if (changeDue >= 10) {
+        change.push(["TEN", cid[6][1]]);
+        changeDue -= cid[6][1];
+    }
+    changeDue = parseFloat(changeDue).toFixed(2);
+
+
+    if (changeDue >= 5 && changeDue <= cid[5][1]) {
+        var quantity = Math.floor((changeDue / 5));
+        change.push(["FIVE", quantity * 5]);
+        changeDue = changeDue - quantity * 5;
+    } else if (changeDue >= 5) {
+        change.push(["FIVE", cid[5][1]]);
+        changeDue -= cid[5][1];
+    }
+    changeDue = parseFloat(changeDue).toFixed(2);
+
+    if (changeDue >= 1 && changeDue <= cid[4][1]) {
+        var quantity = Math.floor(changeDue / 1);
+        change.push(["ONE", quantity * 1]);
+        changeDue = changeDue - quantity * 1;
+    } else if (changeDue >= 1) {
+        change.push(["ONE", cid[4][1]]);
+        changeDue -= cid[4][1];
+    }
+    changeDue = parseFloat(changeDue).toFixed(2);
+
     if (changeDue >= 0.25 && changeDue <= cid[3][1]) {
         var quantity = Math.floor(changeDue / 0.25);
-        var remainder = Math.floor(changeDue % 0.25);
         change.push(["QUARTER", quantity * 0.25]);
-        changeDue = remainder;
+        changeDue = changeDue - quantity * 0.25;
+    } else if (changeDue >= 0.25) {
+        change.push(["QUARTER", cid[3][1]]);
+        changeDue -= cid[3][1];
     }
+    changeDue = parseFloat(changeDue).toFixed(2);
 
     if (changeDue >= 0.1 && changeDue <= cid[2][1]) {
         var quantity = Math.floor(changeDue / 0.1);
-        var remainder = Math.floor(changeDue % 0.1);
         change.push(["DIME", quantity * 0.1]);
-        changeDue = remainder;
+        changeDue = changeDue - quantity * 0.1;
+    } else if (changeDue >= 0.1) {
+        change.push(["DIME", cid[2][1]]);
+        changeDue -= cid[2][1];
     }
+    changeDue = parseFloat(changeDue).toFixed(2);
 
     if (changeDue >= 0.05 && changeDue <= cid[1][1]) {
         var quantity = Math.floor(changeDue / 0.05);
-        var remainder = Math.floor(changeDue % 0.05);
         change.push(["NICKEL", quantity * 0.05]);
-        changeDue = remainder;
+        changeDue = changeDue - quantity * 0.05;
+    } else if (changeDue >= 0.05) {
+        change.push(["NICKEL", cid[1][1]]);
+        changeDue -= cid[1][1];
     }
+    changeDue = parseFloat(changeDue).toFixed(2);
 
     if (changeDue >= 0.01 && changeDue <= cid[0][1]) {
         var quantity = Math.floor(changeDue / 0.01);
-        var remainder = Math.floor(changeDue % 0.01);
         change.push(["PENNY", quantity * 0.01]);
-        changeDue = remainder;
+        changeDue = changeDue - quantity * 0.01;
+    } else if (changeDue >= 0.01) {
+        change.push(["PENNY", cid[0][1]]);
+        changeDue -= cid[0][1];
     }
+    changeDue = parseFloat(changeDue).toFixed(2);
 
 
     return { status: "OPEN", change: change };
