58277c0f68d83896dbc1a4c94c3eeb6a1de69f24
Ganesh H <43777439+ganeshh123@users.noreply.github.com>
2020-09-28 17:54:50 +0100

    quality_assurance_projects

---
+++ quality-assurance/american-british-translator/public/american-to-british-titles.js
@@ -0,0 +1,14 @@
+export const americanToBritishTitles = {
+  'mr.': 'mr',
+  'mrs.': 'mrs',
+  'ms.': 'ms',
+  'mx.': 'mx',
+  'dr.': 'dr',
+  'prof.': 'prof',
+	'Mr.': 'Mr',
+  'Mrs.': 'Mrs',
+  'Ms.': 'Ms',
+  'Mx.': 'Mx',
+  'Dr.': 'Dr',
+  'Prof.': 'Prof'
+}
